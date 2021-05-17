5a0e5c6590dc4b9c049e35a9c8b86f546b3dfc31
Heavy Breathing Cat <35369593+BreathingCat@users.noreply.github.com>
2021-05-17 07:53:49 +0200

    Initial commit

---
+++ app/src/main/java/com/eboshug/hugobosquep2/MainActivity.java
@@ -0,0 +1,108 @@
+package com.eboshug.hugobosquep2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private GameDataHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.toolbar = findViewById(R.id.customToolbar);
+        toolbar.setTitle("App Title");
+        setSupportActionBar(toolbar);
+
+        this.drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, this.drawerLayout, this.toolbar,0,0
+        );
+        this.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        this.navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        this.db = new GameDataHelper(MainActivity.this);
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if(this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.inicio:
+                this.drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+
+            case R.id.carrito:
+                break;
+            case R.id.novedades:
+                break;
+            case R.id.ofertas:
+                break;
+            case R.id.ps4:
+                break;
+            case R.id.xbox:
+                break;
+
+            case R.id.donde_estamos:
+                Intent intent = new Intent(this, DondeEstamosActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                break;
+
+            case R.id.contacto:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.shopAction:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
