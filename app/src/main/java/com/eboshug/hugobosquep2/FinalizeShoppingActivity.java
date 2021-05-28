4f39b914c3e9dab242fcde6741295418832442ee
Heavy Breathing Cat <35369593+BreathingCat@users.noreply.github.com>
2021-05-28 15:11:45 +0200

    Send confirmation email

--- app/src/main/java/com/eboshug/hugobosquep2/FinalizeShoppingActivity.java
+++ app/src/main/java/com/eboshug/hugobosquep2/FinalizeShoppingActivity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FinalizeShoppingActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private Button buy;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +33,35 @@ public class FinalizeShoppingActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        EditText mail = findViewById(R.id.inputMail);
+
+        GameDataHelper db = new GameDataHelper(this);
+        List<VideoGame> carrito = db.getShoppingList();
+
+        float precioFinal = 0;
+        String juegos = "";
+
+        for (int i = 0; i < carrito.size(); i++) {
+            precioFinal = precioFinal + carrito.get(i).getPrecio();
+            juegos = juegos + ", " + carrito.get(i).getNombre();
+        }
+
+        this.buy = findViewById(R.id.buttonBuy);
+        float finalPrecioFinal = precioFinal;
+
+        String finalJuegos = juegos;
+        this.buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, mail.getText());
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Factura de compra en PiumGames");
+                intent.putExtra(Intent.EXTRA_TEXT, "Precio = " + Float.toString(finalPrecioFinal) + "\nItems: " + finalJuegos);
+                startActivity(Intent.createChooser(intent, "Send email"));
+            }
+        });
 
     }
 
@@ -46,6 +82,7 @@ public class FinalizeShoppingActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+        
     }
 
     @Override
